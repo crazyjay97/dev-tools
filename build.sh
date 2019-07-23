544af309758215f2254cbe54756741f6af149232
Professor X <zhanwj@zhiyuaniot.com>
2019-07-23 15:18:26 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#app generate path
+appPath="./app/"
+
+#create folder
+echo "create folder"
+if [[ ! -d "$appPath" ]]; then
+   mkdir app
+fi
+
+#build for mac
+echo "build for mac"
+go build -o ${appPath}/main main.go
+
+#build for windows
+echo "build for windows"
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ${appPath}/main.exe main.go
+
+#build for linux
+echo "build for linux"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ${appPath}/main.out main.go
+
+
+
+#copy dependency
+echo "copy dependency"
+cp -r dist tpl types.ini config.json ${appPath}
+
+#zip
+echo "zip files"
+zip -r code-generator-app.zip ${appPath}
+
+echo "package success"
+
