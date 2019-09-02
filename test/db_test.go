c2cd963ffbd2c6507f5142ce758249fd9deda931
Professor X <zhanwj@zhiyuaniot.com>
2019-09-02 14:29:44 +0800

    代码重构完成

--- test/db_test.go
+++ test/db_test.go
@@ -1,29 +1,29 @@
 package test
 
 import (
-	"code-generator/internal/app"
+	"code-generator/internal/app/db"
 	"fmt"
 	"testing"
 )
 
 func Test_queryTotal(t *testing.T) {
 	//total := db.QueryTotal("")
-	total := app.QueryTotal("sys")
+	total := db.QueryTotal("sys")
 	t.Log(total)
 }
 
 func Test_QueryTable(t *testing.T) {
-	table := app.QueryTable("sys_user")
+	table := db.QueryTable("sys_user")
 	t.Log(table)
 }
 
 func Test_QueryColumns(t *testing.T) {
-	columns := app.QueryColumns("sys_user")
+	columns := db.QueryColumns("sys_user")
 	t.Log(len(*columns))
 }
 
 func Test_QueryList(t *testing.T) {
-	table, count := app.QueryList("", 1, 10)
+	table, count := db.QueryList("", 1, 10)
 	fmt.Println(len(*table))
 	fmt.Println(count)
 }
