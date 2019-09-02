cb6308e8452d55ac0fd2d03415816f61b7c0c937
Professor X <zhanwj@zhiyuaniot.com>
2019-09-02 14:38:47 +0800

    生成异常修复

--- internal/app/gen/generator.go
+++ internal/app/gen/generator.go
@@ -96,7 +96,7 @@ func Gen(config *Config, w http.ResponseWriter) {
 		}
 		for _, tpl := range tpls {
 			bytes, _ := utils.GetFileInProject("asset/tpl/" + tpl.Name + ".tpl")
-			template, _ := pongo2.FromFile(string(bytes))
+			template, _ := pongo2.FromString(string(bytes))
 			rs, _ := template.Execute(data)
 			fW, _ := zipW.Create(getPath(tpl, table.ModuleName, table.FileName, table.ClassName))
 			fW.Write([]byte(rs))
