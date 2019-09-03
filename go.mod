23d974ab1790754be211239cf4faf1a955cc9840
Professor X <zhanwj@zhiyuaniot.com>
2019-09-03 10:59:26 +0800

    优化启动

--- go.mod
+++ go.mod
@@ -5,9 +5,10 @@ go 1.12
 require (
 	github.com/Unknwon/goconfig v0.0.0-20190425194916-3dba17dd7b9e
 	github.com/flosch/pongo2 v0.0.0-20141028000813-5e81b817a0c4
-	github.com/go-sql-driver/mysql v1.4.1
 	github.com/goinggo/mapstructure v0.0.0-20140717182941-194205d9b4a9
 	github.com/jinzhu/gorm v1.9.10
-	github.com/jinzhu/inflection v1.0.0
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/mattn/go-runewidth v0.0.4 // indirect
 	github.com/satori/go.uuid v1.2.0
+	github.com/smartystreets/goconvey v0.0.0-20190731233626-505e41936337 // indirect
 )
--- internal/app/handle/handle.go
+++ internal/app/handle/handle.go
@@ -1,6 +1,7 @@
 package handle
 
 import (
+	"bufio"
 	"code-generator/internal/app/base"
 	"code-generator/internal/app/db"
 	"code-generator/internal/app/gen"
@@ -27,23 +28,27 @@ func init() {
 	http.HandleFunc("/generator/query/columns", queryColumnsHandle)
 	http.HandleFunc("/generator/gen", genHandle)
 	InitHttpProxy()
-	fmt.Println("Server Started Successfully")
+	fmt.Println("Server Starting")
 
 	addrList, err := net.InterfaceAddrs()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+
+	if err := http.ListenAndServe(":"+strconv.Itoa(port), nil); nil != err {
+		fmt.Println("Port Already Used，Press 'Ctrl + C' To Exit")
+		for bufio.NewScanner(os.Stdin).Scan() {
+
+		}
+	}
+
 	for _, address := range addrList {
 		if host, ok := address.(*net.IPNet); ok && !host.IP.IsLoopback() {
 			if host.IP.To4() != nil {
 				fmt.Println("View On:", "http://"+host.IP.String()+":"+strconv.Itoa(port))
 			}
 		}
-	}
-	if err := http.ListenAndServe(":"+strconv.Itoa(port), nil); nil != err {
-		panic("Port Already Used")
-		panic(err)
 	}
 }
 
