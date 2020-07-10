452f15ff333e01f672e287202b4de4f5d9201eb3
zhanweijie <zhanwj@zhiyuaniot.com>
2020-07-10 10:15:54 +0800

    移除注释

--- internal/app/handle/http_proxy.go
+++ internal/app/handle/http_proxy.go
@@ -3,7 +3,6 @@ package handle
 import (
 	"code-generator/internal/app/base"
 	"code-generator/internal/common/utils"
-	"log"
 	"net/http"
 	"strings"
 )
@@ -18,8 +17,16 @@ func InitHttpProxy() {
 			path += DefaultPage
 		}
 		path = AssetRootPath + path
-		log.Print(path)
-		bytes, _ := utils.GetFileInProject(path)
+		bytes, err := utils.GetFileInProject(path)
+		if nil != err {
+			writer.WriteHeader(http.StatusNotFound)
+			writer.Write([]byte(`
+				<body style="text-align:center">
+					<h1>内容失踪了</h1>
+                </body>
+			`))
+			return
+		}
 		suffix := path[strings.LastIndex(path, ".")+1:]
 		fileType, err := base.Types.GetValue("mime", suffix)
 		if nil == err && suffix != "" {
